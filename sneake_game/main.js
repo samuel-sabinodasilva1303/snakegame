42aee5db2e852c7a1d119a82d128a0231f9903ea
Samuel Sabino <sabinodasilvasamuel@gmail.com>
2023-08-15 12:53:35 -0300

    fix: update in all

--- sneake_game/main.js
+++ sneake_game/main.js
@@ -1,53 +1,43 @@
+let pontos = 0;
+let tail = 2;
+let vel = 1; 
+
+
 window.onload = function(){
-      
-    //pegar o elemento canvas pelo ID  
-          let area = document.getElementById('area')
+    let area = document.getElementById('area');
+    let pontosElement = document.getElementById('pontos');
+    let ctx = area.getContext("2d");
+    let vx = 0;
+    let vy = 0;
+    let px = 10;
+    let py = 15;
+    const tp = 20;
+    const qp = 20;
+    let applex = 15;
+    let appley = 15;
+    let trail = [];
     
-    //capturar o quadro de pontuação
-    let pontos = document.getElementById('pontos')
-    let pontos = 0
-    // define o contexto do elemento area para 2d
-          let ctx = area.getContext("2d")
+    document.addEventListener("keydown", keyPush);
+    const easy = document.getElementById('js-easy');
+    const middle = document.getElementById('js-middle');
+    const hard = document.getElementById('js-hard');
+    easy.addEventListener('click', () => setLevel(100));
+    middle.addEventListener('click', () => setLevel(70));
+    hard.addEventListener('click', () => setLevel(50));
+    
+    let gameInterval;
 
-          document.addEventListener("keydown", keyPush)
-    
-    const easy = document.getElementById('js-easy')
-    const middle = document.getElementById('js-middle')
-    const hard = document.getElementById('js-hard')
+    function setLevel(interval) {
+        clearInterval(gameInterval);
+        gameInterval = setInterval(game, interval);
+    }
 
-          setInterval(game, 70)
-
-
-    //Quantidade de casas que a cobra irá andar em cada atualização de quadro
-          const vel = 1
-
-    //Velocidade inicial
-          let vx = 0
-    let vy = 0 
-    
-    //ponto inicial
-          let px = 10
-          let py = 15
-    
-    // Tamanho do ponto
-          const tp = 20
-    
-    // quantidade de pontos
-          const qp = 20
-    
-    // Eixo inicial da Maçã
-          let applex = 15
-    let appley = 15
-
-    // Array para o rastro da cobra
-          let trail = []
-    
+    setLevel(70);
           function game(){
       
               px += vx
               py += vy
       
-      //controle da cobra dentro do quadro para repetição nas bordas
               if (px < 0) {
                   px = qp-1
               }
@@ -62,23 +52,21 @@ window.onload = function(){
               }
 
               ctx.fillStyle = "#111D4A"
-      //sintaxe JavaScript:	contexto.fillRect ( x, y, largura, altura );
               ctx.fillRect(0,0, area.width, area.height)
 
               ctx.fillStyle = "#EA2B1F"
               ctx.fillRect(applex*tp, appley*tp, tp,tp,2,2)
 
-      //for ([expressaoInicial]; [condicao]; [incremento]) declaracao
               for (let i = 0; i < trail.length; i++) {
                 ctx.fillStyle = "#09BC8A"
-        ctx.strokeStyle = "#004346"
+                ctx.strokeStyle = "#004346"
                   ctx.fillRect(trail[i].x*tp, trail[i].y*tp, tp,tp)
-        ctx.strokeRect(trail[i].x*tp, trail[i].y*tp, tp,tp)
+                  ctx.strokeRect(trail[i].x*tp, trail[i].y*tp, tp,tp)
                    if (trail[i].x == px && trail[i].y == py)
                   {
                       vx = vy = 0
                       tail = 2
-          pontoi = 0
+                      pontos = 0
                   }
               }
 
@@ -89,19 +77,18 @@ window.onload = function(){
               }
       
       
-              if (applex==px && appley==py){
-                  tail++
-                  applex = Math.floor(Math.random()*qp)
-                  appley = Math.floor(Math.random()*qp)
-        
-        pontos.innerHTML = ++pontoi
-              }
+              if (applex == px && appley == py) {
+                tail++;
+                applex = Math.floor(Math.random() * qp);
+                appley = Math.floor(Math.random() * qp);
+                
+                pontosElement.innerHTML = ++pontos;
+            }
       
     
           }
 
     window.addEventListener("keydown", function(e) {
-      // space and arrow keys
       if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
           e.preventDefault();
       }
